d1d3966868e7c66ac72ed638b3ed7e4d85a7345e
AfraaAlobaid <afraa.obaid1@gmail.com>
2021-05-03 15:55:10 +0930

    add getMovie function

--- index.js
+++ index.js
@@ -1,36 +1,67 @@
 document.addEventListener("DOMContentLoaded", () => {
-    fetch("https://imdb8.p.rapidapi.com/auto-complete?q=game%20of%20thr", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "4b54f47853mshaa3746e9b0a1c36p16a3b7jsn2c5496e3a6de",
-		"x-rapidapi-host": "imdb8.p.rapidapi.com"
-	}
-})
-.then(response => response.json())
-.then(json => {
-    console.log(json);
-    getMovies(json);
-})
-.catch(err => {
-	console.error(err);
+  let searchInput;
+  document.querySelector("#searchForm").addEventListener("submit", (e) => {
+    e.preventDefault();
+    searchInput = document.querySelector("#searchInput").value;
+    fetch(`https://imdb.p.rapidapi.com/auto-complete?q=${searchInput}`, {
+      method: "GET",
+      headers: {
+        "x-rapidapi-key": "4b54f47853mshaa3746e9b0a1c36p16a3b7jsn2c5496e3a6de",
+        "x-rapidapi-host": "imdb8.p.rapidapi.com",
+      },
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        //console.log(json);
+        getMovies(json);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  });
 });
-})
 
-function getMovies(json){
-    let movies = json.d;
-    let output = "";
-    movies.forEach(movie => {
-        output += `
+function getMovies(json) {
+  let movies = json.d;
+  let output = "";
+  movies.forEach((movie) => {
+    output += `
             <div class="col-md-3">
                 <div class="card bg-secondary">
                     <div class="card-body text-center">
                         <img src="${movie.i.imageUrl}" alt="Movie Cover Image">
                         <p class="m-2">${movie.l}</p>
-                        <a onclick="selectMovie('${movie.id}')" class="btn btn-primary" href="#">Details</a>
+                        <button onclick="selectMovie('${movie.id}')" class="btn btn-primary">Details</button>
                     </div>
                 </div>
             </div>
-        `
+        `;
+  });
+  document.querySelector("#movies").innerHTML = output;
+}
+
+function selectMovie(id) {
+  fetch(
+    `https://imdb-internet-movie-database-unofficial.p.rapidapi.com/film/${id}`,
+    {
+      method: "GET",
+      headers: {
+        "x-rapidapi-key": "4b54f47853mshaa3746e9b0a1c36p16a3b7jsn2c5496e3a6de",
+        "x-rapidapi-host":
+          "imdb-internet-movie-database-unofficial.p.rapidapi.com",
+      },
+    }
+  )
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json);
+      getMovieDetails(json);
+    })
+    .catch((err) => {
+      console.error(err);
     });
-    document.querySelector("#movies").innerHTML = output;
+}
+
+function getMovieDetails(json){
+    
 }
